510f00edb489b29ec85c7f4ef54c178c9d808d83
MukaSchultze <samuelschultze@gmail.com>
2019-06-19 14:17:48 -0300

    Initial commit

---
+++ Assets/Cel.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class Cel {
+
+    public float MaxMass = 1.0f; //The normal, un-pressurized mass of a full water cell
+    public float MaxCompress = 0.02f; //How much excess water a cell can store, compared to the cell above it
+    public float MinMass = 0.0001f; //Ignore cells that are almost dry
+    public float MinFlow = 0f;
+    public float MaxSpeed = 5f;
+
+    //Returns the amount of water that should be in the bottom cell.
+    float get_stable_state_b(float total_mass) {
+        if (total_mass <= 1) {
+            return 1;
+        } else if (total_mass < 2 * MaxMass + MaxCompress) {
+            return (MaxMass * MaxMass + total_mass * MaxCompress) / (MaxMass + MaxCompress);
+        } else {
+            return (total_mass + MaxCompress) / 2;
+        }
+    }
+
+    public void simulate_compression(Vector4[, , ] mass) {
+        float Flow = 0;
+        float remaining_mass;
+
+        var map_width = mass.GetLength(0) - 1;
+        var map_height = mass.GetLength(1) - 1;
+        var map_depth = mass.GetLength(2) - 1;
+
+        for (int x = 0; x < map_width; x++)
+            for (int y = 0; y < map_height; y++)
+                for (int z = 0; z < map_depth; z++)
+                    mass[x, y, z].w = 1f - mass[x, y, z].w;
+
+        var new_mass = (Vector4[, , ])mass.Clone();
+
+        //Calculate and apply flow for each block
+        for (int x = 1; x < map_width; x++)
+            for (int y = 1; y < map_height; y++)
+                for (int z = 1; z < map_depth; z++) {
+                    //Skip inert ground blocks
+                    // if (blocks[x][y] == GROUND)continue;
+                    if (y == 0)continue;
+
+                    //Custom push-only flow
+                    Flow = 0;
+                    remaining_mass = mass[x, y, z].w;
+                    if (remaining_mass <= 0)continue;
+
+                    //The block below this one
+                    // if ((blocks[x, y - 1] != GROUND)) {
+                    if (y > 0) {
+                        Flow = get_stable_state_b(remaining_mass + mass[x, y - 1, z].w) - mass[x, y - 1, z].w;
+                        if (Flow > MinFlow) {
+                            Flow *= 0.5f; //leads to smoother flow
+                        }
+                        Flow = Mathf.Clamp(Flow, 0f, Mathf.Min(MaxSpeed, remaining_mass));
+
+                        new_mass[x, y, z].w -= Flow;
+                        new_mass[x, y - 1, z].w += Flow;
+                        remaining_mass -= Flow;
+                    }
+
+                    if (remaining_mass <= 0f)continue;
+
+                    //Left
+                    // if (blocks[x - 1, y] != GROUND) {
+                    if (x > 0 && x < map_width) {
+                        //Equalize the amount of water in this block and it's neighbour
+                        Flow = (mass[x, y, z].w - mass[x - 1, y, z].w) / 4;
+                        if (Flow > MinFlow) { Flow *= 0.5f; }
+                        Flow = Mathf.Clamp(Flow, 0, remaining_mass);
+
+                        new_mass[x, y, z].w -= Flow;
+                        new_mass[x - 1, y, z].w += Flow;
+                        remaining_mass -= Flow;
+                    }
+
+                    if (remaining_mass <= 0)continue;
+
+                    //Right
+                    // if (blocks[x + 1, y] != GROUND) {
+                    if (x > 0 && x < map_width) {
+                        //Equalize the amount of water in this block and it's neighbour
+                        Flow = (mass[x, y, z].w - mass[x + 1, y, z].w) / 4;
+                        if (Flow > MinFlow) { Flow *= 0.5f; }
+                        Flow = Mathf.Clamp(Flow, 0, remaining_mass);
+
+                        new_mass[x, y, z].w -= Flow;
+                        new_mass[x + 1, y, z].w += Flow;
+                        remaining_mass -= Flow;
+                    }
+
+                    //Forward
+                    // if (blocks[x, y, z + 1] != GROUND) {
+                    if (z > 0 && z < map_depth) {
+                        //Equalize the amount of water in this block and it's neighbour
+                        Flow = (mass[x, y, z].w - mass[x, y, z + 1].w) / 4;
+                        if (Flow > MinFlow) { Flow *= 0.5f; }
+                        Flow = Mathf.Clamp(Flow, 0, remaining_mass);
+
+                        new_mass[x, y, z].w -= Flow;
+                        new_mass[x, y, z + 1].w += Flow;
+                        remaining_mass -= Flow;
+                    }
+
+                    //Back
+                    // if (blocks[x, y, z - 1] != GROUND) {
+                    if (z > 0 && z < map_depth) {
+                        //Equalize the amount of water in this block and it's neighbour
+                        Flow = (mass[x, y, z].w - mass[x, y, z - 1].w) / 4;
+                        if (Flow > MinFlow) { Flow *= 0.5f; }
+                        Flow = Mathf.Clamp(Flow, 0, remaining_mass);
+
+                        new_mass[x, y, z].w -= Flow;
+                        new_mass[x, y, z - 1].w += Flow;
+                        remaining_mass -= Flow;
+                    }
+
+                    if (remaining_mass <= 0)continue;
+
+                    //Up. Only compressed water flows upwards.
+                    // if (blocks[x, y + 1] != GROUND) {
+                    Flow = remaining_mass - get_stable_state_b(remaining_mass + mass[x, y + 1, z].w);
+                    if (Flow > MinFlow) { Flow *= 0.5f; }
+                    Flow = Mathf.Clamp(Flow, 0, Mathf.Min(MaxSpeed, remaining_mass));
+
+                    new_mass[x, y, z].w -= Flow;
+                    new_mass[x, y + 1, z].w += Flow;
+                    remaining_mass -= Flow;
+                    // }
+
+                }
+
+        //Copy the new mass values to the mass array
+        for (int x = 0; x < map_width; x++)
+            for (int y = 0; y < map_height; y++)
+                for (int z = 0; z < map_depth; z++)
+                    mass[x, y, z].w = 1f - new_mass[x, y, z].w;
+
+        // for (int x = 1; x <= map_width; x++) {
+        //     for (int y = 1; y <= map_height; y++) {
+        //         //Skip ground blocks
+        //         if (blocks[x][y] == GROUND)continue;
+        //         //Flag/unflag water blocks
+        //         if (mass[x][y] > MinMass) {
+        //             blocks[x][y] = WATER;
+        //         } else {
+        //             blocks[x][y] = AIR;
+        //         }
+        //     }
+        // }
+
+        //Remove any water that has left the map
+        // for (int x = 0; x < map_width + 2; x++) {
+        //     mass[x][0] = 0;
+        //     mass[x][map_height + 1] = 0;
+        // }
+        // for (int y = 1; y < map_height + 1; y++) {
+        //     mass[0][y] = 0;
+        //     mass[map_width + 1][y] = 0;
+        // }
+
+    }
+
+}
---
+++ Assets/Generator.cs
@@ -0,0 +1,195 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour {
+
+    private Vector4[, , ] vertices;
+    private Mesh mesh;
+
+    [Range(0f, 1f)]
+    public float decrease = 0.5f;
+    [Range(0f, 1f)]
+    public float isoLevel = 0.5f;
+    public bool smooth = true;
+    public bool gizmos = true;
+    public float scale = 0.5f;
+    public Vector3Int cubes = new Vector3Int(15, 15, 15);
+    public Cel c = new Cel();
+
+    private void Awake() {
+        mesh = new Mesh();
+        mesh.MarkDynamic();
+        GetComponent<MeshFilter>().mesh = mesh;
+        GetComponent<MeshCollider>().sharedMesh = mesh;
+    }
+
+    private void OnEnable() {
+        GenerateVertices();
+        UpdateMesh();
+    }
+
+    private void Update() {
+
+        var m = Input.GetMouseButton(0) ? 1 :
+            Input.GetMouseButton(1) ? -1 : 0;
+
+        if (m != 0) {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            var hit = new RaycastHit();
+
+            if (Physics.Raycast(ray, out hit)) {
+                for (var x = 0; x < vertices.GetLength(0); x++)
+                    for (var y = 0; y < vertices.GetLength(1); y++)
+                        for (var z = 0; z < vertices.GetLength(2); z++) {
+                            var us = vertices[x, y, z];
+                            var distance = Vector3.Distance(hit.point, us);
+
+                            if (distance > 2f)
+                                continue;
+
+                            distance /= distance;
+                            distance = Mathf.Min(distance, 0.5f);
+
+                            us.w += m * distance * Time.deltaTime;
+                            vertices[x, y, z] = us;
+                        }
+
+                GetComponent<MeshCollider>().sharedMesh = mesh;
+            }
+        }
+
+        // c.simulate_compression(vertices);
+        UpdateMesh();
+    }
+
+    private void GenerateVertices() {
+        vertices = new Vector4[cubes.x + 1, cubes.y + 1, cubes.z + 1];
+
+        for (var x = 0; x < vertices.GetLength(0); x++)
+            for (var y = 0; y < vertices.GetLength(1); y++)
+                for (var z = 0; z < vertices.GetLength(2); z++) {
+                    // var px = Mathf.PerlinNoise(x * Mathf.PI * scale, 0f / 3f);
+                    // var py = Mathf.PerlinNoise(y * Mathf.PI * scale, 1f / 3f);
+                    // var pz = Mathf.PerlinNoise(z * Mathf.PI * scale, 2f / 3f);
+
+                    // var w = (px + py + pz) / 3f;
+                    // var w = 1f;
+
+                    // if (y == cubes.y - 1)
+                    //     w = 0f;
+
+                    var v = new Vector4(x, y, z, 1f);
+
+                    // var lng = Mathf.Lerp(-180f, 180f, x / (float)cubes.x) * Mathf.Deg2Rad;
+                    // var lat = Mathf.Lerp(-90f, 90f, z / (float)cubes.z) * Mathf.Deg2Rad;
+                    // var height = y;
+
+                    // v.x = Mathf.Cos(lat) * Mathf.Cos(lng);
+                    // v.z = Mathf.Cos(lat) * Mathf.Sin(lng);
+                    // v.y = Mathf.Sin(lat);
+
+                    // v *= height;
+                    v.w = y / (float)cubes.y * Mathf.PerlinNoise(x / (float)cubes.x * scale, z / (float)cubes.z * scale);
+                    // v.w %= 10f;
+
+                    vertices[x, y, z] = v;
+                }
+
+    }
+
+    private void UpdateMesh() {
+
+        var currentIndex = 0;
+        var triangles = new List<Vector3>();
+
+        for (var x = 0; x < vertices.GetLength(0) - 1; x++)
+            for (var y = 0; y < vertices.GetLength(1) - 1; y++)
+                for (var z = 0; z < vertices.GetLength(2) - 1; z++) {
+                    var cubeVertices = new [] {
+                        vertices[x + 0, y + 0, z + 0],
+                        vertices[x + 1, y + 0, z + 0],
+                        vertices[x + 1, y + 0, z + 1],
+                        vertices[x + 0, y + 0, z + 1],
+                        vertices[x + 0, y + 1, z + 0],
+                        vertices[x + 1, y + 1, z + 0],
+                        vertices[x + 1, y + 1, z + 1],
+                        vertices[x + 0, y + 1, z + 1],
+                    };
+
+                    var value = 0;
+
+                    for (var i = 0; i < 8; i++)
+                        value |= (cubeVertices[i].w > isoLevel ? 1 : 0) << i;
+
+                    for (int i = 0; Tables.triangulation[value, i] != -1; i += 3) {
+                        // Get indices of corner points A and B for each of the three edges
+                        // of the cube that need to be joined to form the triangle.
+                        var a0 = Tables.cornerIndexAFromEdge[Tables.triangulation[value, i]];
+                        var b0 = Tables.cornerIndexBFromEdge[Tables.triangulation[value, i]];
+
+                        var a1 = Tables.cornerIndexAFromEdge[Tables.triangulation[value, i + 1]];
+                        var b1 = Tables.cornerIndexBFromEdge[Tables.triangulation[value, i + 1]];
+
+                        var a2 = Tables.cornerIndexAFromEdge[Tables.triangulation[value, i + 2]];
+                        var b2 = Tables.cornerIndexBFromEdge[Tables.triangulation[value, i + 2]];
+
+                        var index = currentIndex++ * 3;
+                        var vertexA = InterpolateVerts(cubeVertices[a0], cubeVertices[b0]);
+                        var vertexB = InterpolateVerts(cubeVertices[a1], cubeVertices[b1]);
+                        var vertexC = InterpolateVerts(cubeVertices[a2], cubeVertices[b2]);
+
+                        triangles.Add(vertexB);
+                        triangles.Add(vertexA);
+                        triangles.Add(vertexC);
+                    }
+                }
+
+        var v = new Vector3[triangles.Count];
+        var meshTriangles = new int[triangles.Count];
+
+        for (int i = 0; i < triangles.Count; i++) {
+            meshTriangles[i] = i;
+            v[i] = triangles[i];
+        }
+
+        mesh.Clear();
+        mesh.vertices = v;
+        mesh.triangles = meshTriangles;
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+    }
+
+    private Vector3 InterpolateVerts(Vector4 v1, Vector4 v2) {
+        if (smooth) {
+            float t = (isoLevel - v1.w) / (v2.w - v1.w);
+            return v1 + t * (v2 - v1);
+        } else {
+            return (v1 + v2) / 2f;
+        }
+    }
+
+    private void OnValidate() {
+        if (mesh) {
+            GenerateVertices();
+            UpdateMesh();
+        }
+    }
+
+    private void OnDrawGizmos() {
+        if (vertices == null || !gizmos)
+            return;
+
+        for (var x = 0; x < vertices.GetLength(0); x++)
+            for (var y = 0; y < vertices.GetLength(1); y++)
+                for (var z = 0; z < vertices.GetLength(2); z++) {
+                    var v = vertices[x, y, z];
+                    Gizmos.color = Color.Lerp(Color.black, Color.white, v.w);
+
+                    if (v.w > isoLevel)
+                        Gizmos.DrawSphere(v, 0.05f);
+                    else
+                        Gizmos.DrawCube(v, Vector3.one * 2f * 0.05f);
+                }
+    }
+
+}
